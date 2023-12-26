dae2e9a8c7aaa0dbe64414526f1f5e756126986d
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-12-27 00:33:13 +0600

    Project created and assets added

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,26 @@
+//
+//
+// FileName: ViewController.swift
+// ProjectName: Clima
+//
+// Created by MD ABIR HOSSAIN on 27-12-2023 at 12:25 AM
+// Website: https://mdabirhossain.com/
+//
+
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
